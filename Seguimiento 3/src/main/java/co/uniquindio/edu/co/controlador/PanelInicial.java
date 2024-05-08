d9a2e0cdb212c2f896922fb45a077848bc574c76
Viverosj <141973610+Viverosj@users.noreply.github.com>
2024-05-07 23:54:36 -0500

    Update PanelInicial.java

--- Seguimiento 3/src/main/java/co/uniquindio/edu/co/controlador/PanelInicial.java
+++ Seguimiento 3/src/main/java/co/uniquindio/edu/co/controlador/PanelInicial.java
@@ -1,4 +1,39 @@
 package co.uniquindio.edu.co.controlador;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class PanelInicial {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button CambiarEstado;
+
+    @FXML
+    private Button ConsultarEnvio;
+
+    @FXML
+    private Button CrearEnvio;
+
+    @FXML
+    private TableView<?> Informacion;
+
+    @FXML
+    void initialize() {
+        assert CambiarEstado != null : "fx:id=\"CambiarEstado\" was not injected: check your FXML file 'panelInicial.fxml'.";
+        assert ConsultarEnvio != null : "fx:id=\"ConsultarEnvio\" was not injected: check your FXML file 'panelInicial.fxml'.";
+        assert CrearEnvio != null : "fx:id=\"CrearEnvio\" was not injected: check your FXML file 'panelInicial.fxml'.";
+        assert Informacion != null : "fx:id=\"Informacion\" was not injected: check your FXML file 'panelInicial.fxml'.";
+
+    }
+
 }
+
